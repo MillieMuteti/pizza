0a3a4f3ef9d52dcb8ef5b4bc420b33005bac894f
Millie Muteti <milliemutetigmail.com>
2019-07-28 00:10:01 +0300

    Re-design html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,16 @@
-//business logic
-// function Pizzasize(mega,large,medium,small){
-//     this.mega=mega;
-//     this.large=large;
-//     this.medium=medium;
-//     this.small = small;
-// }
-// function ptoppings(none,pepporoni,mushroom,sausage,onions,pineapples,extracheese,greenpepper,blackolives){
-//     this.none = none;
-//     this.pepporoni = pepporoni;
-//     this.mushroom = mushroom;
-//     this.sausage = sausage;
-//     this.onions = onions;
-//     this.pineapples = pineapples;
-//     this.extracheese = extracheese;
-//     this.greenpepper = greenpepper;
-//     this.blackolives = blackolives;
-// }
-function Pizza(size,toppings,crust,quantity){
+let cart = [];
+
+let newOrdercart=[]
+
+function Pizza(size,toppings,crust,quantity,type){
     this.size=size;
     this.crust=crust;
     this.toppings=toppings;
     this.quantity=quantity;
-
+    this.type=type;
 }
 
-
+ 
 //user interface logic
 $(document).ready(function(){ 
     $("#add").click(function(e){
@@ -33,9 +19,11 @@ $(document).ready(function(){
         var pQuant = $("#quantity").val();
         var pCrust = $("#crust").val();
         var pTop = $("#toppings").val();
-        var newOrder=new Pizza(pSize,pTop,pCrust,pQuant);
-
-    
+        var pType=$("input[type=checked").val();
+        var newOrder=new Pizza(pSize,pTop,pCrust,pQuant,pType);
+        cart.push(newOrder)
+        console.log(cart)
         
     });
+
 });
